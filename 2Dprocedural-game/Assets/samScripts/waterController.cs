150448366162857285af3d869089fcf047015e1d
Samuel Mennega <scm32@calvin.edu>
2022-10-25 16:35:03 -0400

    Update waterController.cs

--- 2Dprocedural-game/Assets/samScripts/waterController.cs
+++ 2Dprocedural-game/Assets/samScripts/waterController.cs
@@ -6,10 +6,21 @@ public class waterController : MonoBehaviour
 {
 
     private BoxCollider2D bc;
+    public GameObject playerObject;
+    private Rigidbody2D playerRB;
+    private playerController player;
+    private float initialDrag;
+    private float initialGravity;
     // Start is called before the first frame update
     void Start()
     {
         bc = GetComponent<BoxCollider2D>();
+        playerRB = playerObject.GetComponent<Rigidbody2D>();
+        player = playerObject.GetComponent<playerController>();
+        initialDrag = playerRB.drag;
+        initialGravity = playerRB.gravityScale;
+
+
     }
 
     // Update is called once per frame
@@ -22,10 +33,22 @@ public class waterController : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            playerController player = collision.GetComponent<playerController>();
-            Rigidbody2D playerRB = collision.GetComponent<Rigidbody2D>();
+          
             player.myState = playerController.playerState.Landed;
             playerRB.drag = 10;
+            playerRB.gravityScale = 0;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = collision.GetComponent<playerController>();
+            playerRB = collision.GetComponent<Rigidbody2D>();
+            playerRB.drag = initialDrag;
+            playerRB.gravityScale = initialGravity;
 
         }
     }
