9f0450a9388504708f5c07d5937fe42e96b72692
Samuel Mennega <scm32@calvin.edu>
2022-10-25 15:32:26 -0400

    Minimum Viable Produce

---
+++ 2Dprocedural-game/Assets/samScripts/cameraController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    [Header("references")]
+    public GameObject playerObject;
+    public Transform launchPos;
+
+    [Header("camera options")]
+    public float smoothTime;
+    public Vector3 launchOffset = new Vector3(0, 0, -10f);
+
+
+    private Vector3 playerPosition;
+    private playerController player;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        playerPosition = playerObject.transform.position;
+        player = playerObject.GetComponent<playerController>();
+
+        if(player.myState == playerController.playerState.Idle || player.myState == playerController.playerState.PullBack)
+        {
+            gameObject.transform.position = launchPos.position + launchOffset;
+        }
+
+        else
+        {
+            gameObject.transform.position = Vector3.SmoothDamp(gameObject.transform.position, new Vector3(playerPosition.x, playerPosition.y, -10f), ref velocity,smoothTime);
+        }
+    }
+}
---
+++ 2Dprocedural-game/Assets/samScripts/parallax.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    private float  startpos;
+    public GameObject camera;
+    public float parallaxFactor;
+    void Start()
+    {
+        startpos = transform.position.x;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (camera.transform.position.x * parallaxFactor);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
