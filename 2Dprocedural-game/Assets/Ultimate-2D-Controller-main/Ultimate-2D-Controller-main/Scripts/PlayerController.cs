19efda908c9166d6fc11992d5c9cc2b57b002408
Samuel Mennega <scm32@calvin.edu>
2022-10-31 19:42:32 -0400

    s

--- 2Dprocedural-game/Assets/Ultimate-2D-Controller-main/Ultimate-2D-Controller-main/Scripts/PlayerController.cs
+++ 2Dprocedural-game/Assets/Ultimate-2D-Controller-main/Ultimate-2D-Controller-main/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ namespace TarodevController {
         void Activate() =>  _active = true;
 
         private void Update() {
-            if (!_active || !mapGenerator.launch) return;
+            if (!_active ||(!mapGenerator.launch && Grounded)) return;
             // Calculate velocity
             Velocity = (transform.position - _lastPosition) / Time.deltaTime;
             _lastPosition = transform.position;
