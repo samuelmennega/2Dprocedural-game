aa3adcf1a071747dcc3497dd73858a89aa6071ee
Samuel Mennega <scm32@calvin.edu>
2022-10-31 16:21:57 -0400

    added chunks (not working)

--- 2Dprocedural-game/Assets/samScripts/mapGenerator.cs
+++ 2Dprocedural-game/Assets/samScripts/mapGenerator.cs
@@ -6,28 +6,43 @@ public class mapGenerator : MonoBehaviour
 {
     private GameObject currentBiome;
     public Biome[] biomes;
-    public Vector2 startingBackgroundLoc;
-    public Vector2 startingGroundLoc;
-    public int scrollSpeedInitial;
-    public float decel;
+
+    [Header("initialization")]
+    public int mapLength;
+    public static int chunkSize;
+    public GameObject chunkPrefab;
+    public static Vector2 startingBackgroundLoc;
+    public static Vector2 startingGroundLoc;
+
+    [Header("speed")]
+    public static int scrollSpeedInitial;
+    public static float decel;
     private float backgroundWidth;
     private float groundWidth;
     private GameObject[] launchObjects = new GameObject[4];
 
 
 
+
     private void Awake()
     {
-        int i = 0;
-        GameObject newBackground = launchObjects[i++]= Instantiate(biomes[0].background, startingBackgroundLoc, Quaternion.identity);
-        GameObject newGround = launchObjects[i++] = Instantiate(biomes[0].ground, startingGroundLoc, Quaternion.identity);
+        //instantiate chunks
+        int numChunks = (mapLength / chunkSize) + 1;
+        for(int i = 0; i <= numChunks; i++)
+        {
+            Instantiate(chunkPrefab, new Vector3(startingBackgroundLoc.x, startingBackgroundLoc.y, 0) + transform.right * chunkSize * i, Quaternion.identity);
+        }
+        //initialize background and ground
+        int index = 0;
+        GameObject newBackground = launchObjects[index++]= Instantiate(biomes[0].background, startingBackgroundLoc, Quaternion.identity);
+        GameObject newGround = launchObjects[index++] = Instantiate(biomes[0].ground, startingGroundLoc, Quaternion.identity);
        
         backgroundWidth = newBackground.GetComponent<backgroundMovement>().repeatWidth;
         groundWidth = newGround.GetComponent<backgroundMovement>().repeatWidth;
 
 
-        launchObjects[i++] = Instantiate(biomes[0].ground, (startingGroundLoc + Vector2.right * backgroundWidth), Quaternion.identity);
-        launchObjects[i++] = Instantiate(biomes[0].background, (startingBackgroundLoc + Vector2.right * backgroundWidth), Quaternion.identity);
+        launchObjects[index++] = Instantiate(biomes[0].ground, (startingGroundLoc + Vector2.right * backgroundWidth), Quaternion.identity);
+        launchObjects[index++] = Instantiate(biomes[0].background, (startingBackgroundLoc + Vector2.right * backgroundWidth), Quaternion.identity);
 
     }
 
